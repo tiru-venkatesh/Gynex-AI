0669a500256b368b0474970c757fb32451215c4a
tiru_venkatesh <tiruvenkatesh123@gmail.com>
2026-02-07 13:25:09 +0530

    Update config.ts

--- img2xl-preplexity-dummy-main/src/api/config.ts
+++ img2xl-preplexity-dummy-main/src/api/config.ts
@@ -1,2 +1,3 @@
 // src/api/config.ts
-export const API_BASE_URL = "gynex-ai-production.up.railway.app";
+export const AI_URL = "gynex-ai-production.up.railway.app";
+export const AUTH_URL = "https://your-auth-backend.up.railway.app";
